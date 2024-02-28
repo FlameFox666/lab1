b9263e801f0c5e1356b4fa273bf86c9018f89dc7
FlameFox666 <lexarole@gmail.com>
2024-02-28 17:16:44 +0200

    foreach cycle

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-    for ($q = 0; $q <= 100; $q++) {
-        if ($q % 2 == 0) {
-            echo $q.PHP_EOL;
+    $a = [1, 23, 44, 88, 13, 11, 7, 8];
+
+    foreach ($a as $x){
+        if ($x % 2 == 0) {
+            echo $x.PHP_EOL;
         }
-      }
+    }
 ?>
